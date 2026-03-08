08d8a2ae4ede33a5a7fb5cab42366d0b27120414
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-08 17:08:06 +0100

    Zadanie2 lab2

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,17 @@ public class Main{
 //        System.out.println(point2.toString());
 //
 //    }
-    public static Segment longest(Segment[] segments) {
-        Segment longest = segments[0];
-        for(Segment s : segments) {
-            if(s.length() > longest.length()) {
-                longest = s;
-            }
-        }
-        return longest;
-    }
+
     public static void main(String[] args) {
         Segment[] segments = {
                 new Segment(new Point(0,0), new Point(3,4)),
                 new Segment(new Point(1,1), new Point(5,5)),
                 new Segment(new Point(2,2), new Point(10,2))
         };
-        Segment longest = longest(segments);
-        System.out.println("The longest: " + longest.length());
+
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 4);
+        Segment segment = new Segment(p1, p2);
+        System.out.println(segment);
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,34 +1,42 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Point {
 
     private double x = 5;
-    private double y  = 10;
+    private double y = 10;
+
     public Point() {
-        x =0;
-        y =0;
+        x = 0;
+        y = 0;
     }
-    public Point(double x, double y){
+
+    public Point(double x, double y) {
         this.x = x;
-        this.y = y;
-    }
-    public double getX() {
-        return x;
-    }
-    public double getY() {
-        return y;
-    }
-    public void setX(double x) {
-        this.x = x;
-    }
-    public void setY(double y) {
         this.y = y;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 
 
     public String toString() {
         String napis = "x=" + this.x + " y=" + this.y;
         return napis;
     }
+
     public String toSvg() {
         String napis = "<circle r=\"45\" cx=\"" + x
                 + "\" cy=\"" + y + "\" fill=\"red\"";
@@ -39,6 +47,7 @@ public class Point {
         this.x += dx;
         this.y += dy;
     }
+
     public Point translated(double dx, double dy) {
         Point point = new Point();
         point.x += dx;
