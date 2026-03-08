71778c91b275beecd3583a4a366ca1c8a8573360
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-08 17:37:26 +0100

    Zadanie4 lab2

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,12 +1,16 @@
 public class Polygon {
     private Point[] points;
     public Polygon(Point[] points) {
-        this.points = points;
+        this.points = new Point[points.length];
+
+        for(int i=0; i< points.length; i++) {
+            this.points[i] = new Point(points[i].getX(), points[i].getY());
+        }
     }
     public String toString() {
         String res = "Punkty wielokata: ";
         for(Point p: points) {
-            res += "(" + p.getX() + " , " + p.getY() + ")";
+            res = res + "(" + p.getX() + " , " + p.getY() + ")";
         }
         return res;
     }
@@ -18,4 +22,12 @@ public class Polygon {
         svg += "\" />";
         return svg;
     }
+    public Polygon(Polygon other) {
+        this.points = new Point[other.points.length];
+        for(int i = 0; i < other.points.length; i++) {
+            this.points[i] = new Point(other.points[i].getX(),other.points[i].getY());
+        }
+    }
+
+
 }
