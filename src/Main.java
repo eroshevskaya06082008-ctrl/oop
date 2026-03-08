6b1afdc50e86bb96850cb9cf222affd592dc7702
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-08 14:40:13 +0100

    Zadanie1 lab1

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,12 @@
 
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        Point point  = new Point();
+        System.out.println(point.x + " " + point.y);
+        point.x = 7;
+        System.out.println(point.x + " " + point.y);
+        Point point1 = new Point();
+        System.out.println(point1.toString());
+    }
+}
---
+++ src/Point.java
@@ -0,0 +1,8 @@
+public class Point {
+    public double x = 5;
+    public double y = 10;
+    public String toString() {
+        String napis = "x=" + this.x + " y=" + this.y;
+        return napis;
+    }
+}
