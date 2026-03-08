6a4ea504c4049d643e687468ce2f8edaacc973b5
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-08 14:49:21 +0100

    Zadanie2 lab1

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main{
         System.out.println(point.x + " " + point.y);
         Point point1 = new Point();
         System.out.println(point1.toString());
-
+        System.out.println(point1.toSvg());
 
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -4,7 +4,10 @@ public class Point {
     public String toString() {
         String napis = "x=" + this.x + " y=" + this.y;
         return napis;
-
-
+    }
+    public String toSvg() {
+        String napis = "<circle r=\"45\" cx=\"" + x
+                + "\" cy=\"" + y + "\" fill=\"red\"";
+        return napis;
     }
 }
