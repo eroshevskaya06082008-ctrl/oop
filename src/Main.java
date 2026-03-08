d18b956a3d5d24d9b848821eeee5888cfce40525
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-08 15:27:03 +0100

    Zadanie5 lab1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
 
 public class Main{
+//    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//
+//        Point point = new Point(5, 7);
+//        System.out.println(point.getX() + " " + point.getY());
+//        point.setX(7);
+//        System.out.println(point.getX() + " " + point.getY());
+//        Point point2 = new Point();
+//        System.out.println(point2.toString());
+//
+//    }
+    public static Segment longest(Segment[] segments) {
+        Segment longest = segments[0];
+        for(Segment s : segments) {
+            if(s.length() > longest.length()) {
+                longest = s;
+            }
+        }
+        return longest;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        Point point = new Point(5, 7);
-        System.out.println(point.getX() + " " + point.getY());
-        point.setX(7);
-        System.out.println(point.getX() + " " + point.getY());
-        Point point2 = new Point();
-        System.out.println(point2.toString());
-
+        Segment[] segments = {
+                new Segment(new Point(0,0), new Point(3,4)),
+                new Segment(new Point(1,1), new Point(5,5)),
+                new Segment(new Point(2,2), new Point(10,2))
+        };
+        Segment longest = longest(segments);
+        System.out.println("The longest: " + longest.length());
     }
 }
