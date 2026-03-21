75a512950daa3828a2d91a850b5908ca97508e31
Hanna Yerasheuskaya <eroshevskaya06082008@gmail.com>
2026-03-21 14:32:13 +0100

    Zadanie7 lab2

---
+++ src/BoundingBox.java
@@ -0,0 +1,9 @@
+public record BoundingBox(double x, double y, double width, double height) {
+    /*Zadanie 7.
+
+Zdefiniuj rekord BoundingBox składający się z czterech liczb zmiennoprzecinkowych: położenia x i y
+lewego górnego punktu prostokątnego obrysu wielokąta, a także szerokości oraz wysokości tego obrysu.
+W klasie Polygon napisz metodę boundingBox(), która zwróci obiekt BoundingBox opisujący dany wielokąt.*/
+
+
+}
--- src/Polygon.java
+++ src/Polygon.java
@@ -1,32 +1,47 @@
 public class Polygon {
-    private Point[] points;
-    public Polygon(Point[] points) {
-        this.points = new Point[points.length];
 
-        for(int i=0; i< points.length; i++) {
-            this.points[i] = new Point(points[i].getX(), points[i].getY());
-        }
+    private Point[] points;
+    public Polygon(Point[] points){
+       this.points = new Point[points.length];
+       for(int i = 0; i< points.length;i++) {
+           this.points[i] = new Point(points[i].getX(), points[i].getY());
+       }
     }
     public String toString() {
         String res = "Punkty wielokata: ";
-        for(Point p: points) {
-            res = res + "(" + p.getX() + " , " + p.getY() + ")";
+        for(Point p : points) {
+            res += "( " + p.getX() + " , " + p.getY() + ")";
         }
         return res;
     }
     public String toSvg() {
-        String svg = "<polygon points=\"";
+        String svg = "<polygon points=\\";
         for(Point p : points) {
-            svg += p.getX() + "," + p.getY()+ " ";
+            svg += p.getX() + ", " + p.getY() + " ";
         }
         svg += "\" />";
         return svg;
     }
     public Polygon(Polygon other) {
         this.points = new Point[other.points.length];
-        for(int i = 0; i < other.points.length; i++) {
-            this.points[i] = new Point(other.points[i].getX(),other.points[i].getY());
+        for(int i = 0; i< other.points.length; i++) {
+            this.points[i] = new Point(other.points[i].getX(), other.points[i].getY());
         }
+    }
+    public BoundingBox boundingBox() {
+        double minX = points[0].getX();
+        double minY = points[0].getY();
+        double maxX = points[0].getX();
+        double maxY = points[0].getY();
+        for (Point p : points) {
+            if(minX > p.getX()) {minX = p.getX();}
+            if(minY > p.getY()) {minY = p.getY();}
+            if(maxX < p.getX()) {maxX = p.getX();}
+            if(maxY < p.getY()) {maxY = p.getY();}
+        }
+        double width = maxX - minX;
+        double height = maxY - minY;
+        return new BoundingBox(minX, minY, width, height);
     }
 
 
